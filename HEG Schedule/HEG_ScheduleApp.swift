b900e0d2d7f56380387b3fde2045da2e7a9a3301
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-05-25 21:19:35 +0200

    Initial Commit

---
+++ HEG Schedule/HEG_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HEG_ScheduleApp.swift
+//  HEG Schedule
+//
+//  Created by Dietrich Poensgen on 25.05.21.
+//
+
+import SwiftUI
+
+@main
+struct HEG_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
